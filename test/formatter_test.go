b99289c2e44f75589a9bec04974c3d8f3c595173
BubbleFish <daforsastudia@gmail.com>
2025-11-04 13:36:40 +0300

    test: Add test for formatter

--- test/formatter_test.go
+++ test/formatter_test.go
@@ -1,6 +1,7 @@
 package parser_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -8,20 +9,41 @@ import (
 	"gotest.tools/assert"
 )
 
+type FormatterTestCase struct {
+	Input  string
+	Output []libparser.FormatPart
+}
+
+var FormatterTestCases = []FormatterTestCase{
+	{
+		Input: "hello $message and others!",
+		Output: []libparser.FormatPart{
+			libparser.LiteralFormat{
+				Literal: "hello ",
+			},
+			libparser.VariableFormat{
+				Name:     "message",
+				Modifier: nil,
+			},
+			libparser.LiteralFormat{
+				Literal: " and others!",
+			},
+		},
+	},
+}
+
 func TestFormatter(test *testing.T) {
-	formatter := libparser.NewFormatter(strings.NewReader("hello $message and others!"))
-	parts, err := formatter.Parse()
-	assert.NilError(test, err)
-	assert.DeepEqual(test, parts, []libparser.FormatPart{
-		libparser.LiteralFormat{
-			Literal: "hello ",
-		},
-		libparser.VariableFormat{
-			Name:     "message",
-			Modifier: nil,
-		},
-		libparser.LiteralFormat{
-			Literal: " and others!",
-		},
-	})
+	for i, test_case := range FormatterTestCases {
+		test.Run(
+			fmt.Sprintf("%02d_with_%d_characters", i, len(test_case.Input)),
+			func(test *testing.T) {
+				formatter := libparser.NewFormatter(strings.NewReader(test_case.Input))
+
+				parts, err := formatter.Parse()
+				assert.NilError(test, err)
+
+				assert.DeepEqual(test, parts, test_case.Output)
+			},
+		)
+	}
 }
