496271f8e1ba6dd5afcf5e76061332d5809e8a8d
BubbleFish <daforsastudia@gmail.com>
2025-11-12 17:01:15 +0300

    feat: Add OpenNew() to parse a file

---
+++ parser_file.go
@@ -0,0 +1,32 @@
+package libparser
+
+import (
+	"bufio"
+	"os"
+)
+
+type CanClose interface {
+	Close() error
+}
+
+var OpenedSources []CanClose
+
+func OpenNew(path string, processors ...PostProcessor) (*Parser, error) {
+	file, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	OpenedSources = append(OpenedSources, file)
+
+	return New(path, bufio.NewReader(file), processors...), nil
+}
+
+// Closes all files opened using [OpenNew()].
+//
+// Recommended to use `defer parser.Close()` in your main function.
+func (parser *Parser) Close() {
+	for _, file := range OpenedSources {
+		file.Close()
+	}
+	OpenedSources = []CanClose{}
+}
--- test/parser_test.go
+++ test/parser_test.go
@@ -1,9 +1,7 @@
 package libparser_test
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"path/filepath"
 	"testing"
 
@@ -13,16 +11,13 @@ import (
 
 func TestAll(test *testing.T) {
 	for _, test_case := range ExpectedData {
-		file, err := os.OpenFile(
-			filepath.Join("data", test_case.Filename),
-			os.O_RDONLY,
-			os.ModePerm,
-		)
-		assert.NilError(test, err)
-		defer file.Close()
+		path := filepath.Join("data", test_case.Filename)
 
-		test.Run(file.Name(), func(test *testing.T) {
-			parser := libparser.New(file.Name(), bufio.NewReader(file), libparser.PostNoShebang)
+		test.Run(test_case.Filename, func(test *testing.T) {
+			parser, err := libparser.OpenNew(path, libparser.PostNoShebang)
+			assert.NilError(test, err)
+			defer parser.Close()
+
 			tree, parser_err := parser.Parse()
 			if parser_err != nil {
 				fmt.Println(parser_err.GetBeautyPrinted())
@@ -37,17 +32,10 @@ func TestAll(test *testing.T) {
 func TestPostProcessor(test *testing.T) {
 	message := "this is a result of post processing!"
 
-	file, err := os.OpenFile(
-		filepath.Join("data", "01_basic.tome"),
-		os.O_RDONLY,
-		os.ModePerm,
-	)
-	assert.NilError(test, err)
-	defer file.Close()
+	path := filepath.Join("data", "01_basic.tome")
 
-	parser := libparser.New(
-		file.Name(),
-		bufio.NewReader(file),
+	parser, err := libparser.OpenNew(
+		path,
 		libparser.PostNoShebang,
 		func(node libparser.Node) (libparser.Node, *libparser.DetailedError) {
 			switch node := node.(type) {
@@ -59,6 +47,9 @@ func TestPostProcessor(test *testing.T) {
 		libparser.PostExclude[*libparser.ExecNode],
 		libparser.PostExclude[*libparser.DirectiveNode],
 	)
+	assert.NilError(test, err)
+	defer parser.Close()
+
 	tree, parser_err := parser.Parse()
 	if parser_err != nil {
 		fmt.Println(parser_err.GetBeautyPrinted())
